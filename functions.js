fcc0421ae05348083be91e97f00ac9fa910616b0
Alx-404 <alexanderreis70@gmail.com>
2021-04-15 20:48:54 -0300

    first commit

---
+++ functions.js
@@ -0,0 +1,50 @@
+lc="abcdefghijklmnopqrstuvwyz"
+uc="ABCDEFGHIJKLMNOPQRSTUVWYZ"
+di="1234567890"
+sp="!@#$%&*ç{}[]/"
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function copyPassword(){
+    navigator.clipboard.writeText(document.getElementById("passwordprint").value);
+}
+
+function generatePassword(){
+    selectedChars=""
+    selectedFeatures= []
+
+    var length = document.getElementById("length").value
+
+    var chkDigitis = document.getElementById("digitis").checked
+    var chkUppercase = document.getElementById("uppercase").checked
+    var chkLowercase = document.getElementById("lowercase").checked
+    var chkSpecialChars = document.getElementById("specialchars").checked
+    
+    
+    if(chkDigitis){
+        selectedChars+=di
+        selectedFeatures.push(di)
+    }if(chkUppercase){
+        selectedChars+=uc
+        selectedFeatures.push(uc)
+    }if(chkLowercase){
+        selectedChars+=lc
+        selectedFeatures.push(lc)
+    }if(chkSpecialChars){
+        selectedChars+=sp
+        selectedFeatures.push(sp)
+    }
+    
+    password=""
+    for (var i=1;i<=length;i++){
+        char=selectedChars[getRandomInt(1,selectedChars.length)]
+        password+=char
+    }
+
+    document.getElementById("passwordprint").value=password
+}
+            
